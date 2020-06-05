ec1b097a2498f6506bf8d3b450f329494c2e0c4d
bwhewell <ben.whewell@engineer.com>
2020-06-05 11:44:29 -0400

    removed pycache, set layer sizes

--- initialize.py
+++ initialize.py
@@ -14,8 +14,8 @@ class eigen_djinn:
         from discrete1.util import chem,sn_tools
         import numpy as np
         
-        distance = [0.69,0.17,0.3,0.17,0.69]
-        delta = 0.001 # original spatial cell width
+        distance = [50,35,40,35,50]
+        delta = 0.2 # original spatial cell width
         if conc is None:
             conc = 0.2
         print('Concentration: ',conc)
@@ -76,8 +76,8 @@ class eigen_djinn:
     def boundaries(conc):
         # import numpy as np
         from discrete1.util import sn_tools
-        distance = [0.69,0.17,0.3,0.17,0.69]
-        delta = 0.001
+        distance = [50,35,40,35,50]
+        delta = 0.2
         layers = [int(ii/delta) for ii in distance]
         splits = sn_tools.layer_slice(layers)
         enrichment = sn_tools.enrich_list(sum(layers),conc,[splits[1],splits[2]])
