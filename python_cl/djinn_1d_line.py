3c163319944d3bf965c34fe066791cae3a89d6c3
Benji Whewell <ben.whewell@engineer.com>
2020-06-17 16:40:24 -0400

    Command Line DJINN Model Search

--- python_cl/djinn_1d_line.py
+++ python_cl/djinn_1d_line.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import numpy as np
-from djinn import djinn
+from djinn2 import djinn
 import sklearn
 import argparse
-from whewell import auto_mail
 
 parser = argparse.ArgumentParser(description='Which Search')
 parser.add_argument('-e',action='store',dest='en')
+parser.add_argument('-l',action='store',dest='lab')
 usr_input = parser.parse_args()
 
 if usr_input.en == 's':
@@ -19,19 +19,30 @@ elif usr_input.en == 'f':
 
 
 mymat = np.load('mydata/djinn_true_1d/true_track_{}_combined.npy'.format(file1))
-X = mymat[:,:2,:].copy() #check this
-X = np.hstack((X[:,0][:,:1],X[:,1]))
-Y = mymat[:,2,:].copy()
-
+if usr_input.lab is None:
+    print('Non Labeled Data')
+    X = mymat[:,1,:].copy() #check this
+    Y = mymat[:,2,:].copy()
+    file2 = ''
+else:
+    print('Labeled Data')
+    X = mymat[:,:2,:].copy() #check this
+    X = np.hstack((X[:,0][:,:1],X[:,1]))
+    Y = mymat[:,2,:].copy()
+    file2 = '_enrich'
+    
 num_trees = [1,3,5] 
 num_depth = [2,4,6] 
 
 split = 0.2
-spatial = np.arange(len(mymat))
-np.random.shuffle(spatial)
-np.save('mydata/djinn_{}_1d/spatialShuffleMat'.format(file1),spatial)
-index = int(len(spatial)*split)
+try:
+    spatial = np.load('mydata/djinn_{}_1d/spatialShuffleMat'.format(file1))
+except FileNotFoundError:
+    spatial = np.arange(len(mymat))
+    np.random.shuffle(spatial)
+    np.save('mydata/djinn_{}_1d/spatialShuffleMat'.format(file1),spatial)
 
+index = int(len(spatial)*split)
 x_train = X[spatial[index:]]; y_train = Y[spatial[index:]]
 x_test = X[spatial[:index]]; y_test = Y[spatial[:index]]
 
@@ -41,7 +52,7 @@ for jj in num_trees:
         print("ntrees:",jj,"maxdepth:",ii)
         print('='*25)
         z_jj = str(jj).zfill(3); z_ii = str(ii).zfill(3)
-        modelname="{}_enrich_1d_djinn_model/model_{}{}".format(file1,z_jj,z_ii)    # name the model
+        modelname="{}{}_1d_djinn_model/model_{}{}".format(file1,file2,z_jj,z_ii)    # name the model
         ntrees=jj                        # number of trees = number of neural nets in ensemble
         maxdepth=ii                      # max depth of tree -- optimize this for each data set
         dropout_keep=1.0                # dropout typically set to 1 for non-Bayesian models
@@ -64,7 +75,7 @@ for jj in num_trees:
         m=model.predict(x_test)
 
         # evaluate results
-        with open('{}_enrich_1d_djinn_model/error/model_ntrees{}_maxdepth{}.txt'.format(file1,z_jj,z_ii),'a') as f:
+        with open('{}{}_1d_djinn_model/error/model_ntrees{}_maxdepth{}.txt'.format(file1,file2,z_jj,z_ii),'a') as f:
             for kk in range(y_test.shape[0]):
                 mse=sklearn.metrics.mean_squared_error(y_test[kk],m[kk])
                 mabs=sklearn.metrics.mean_absolute_error(y_test[kk],m[kk])
@@ -72,6 +83,5 @@ for jj in num_trees:
                 f.write('MSE '+str(mse)+' M Abs Err '+str(mabs)+' Expl. Var. '+str(exvar)+'\n')
         # close model 
         model.close_model()
-
-auto_mail.send("DJINN Finished - {}".format(file1))       
+   
     
