26a0a055ef644b0a6d371204c85193233a93a98d
Ben <ben.whewell@engineer.com>
2020-08-10 12:06:57 -0400

    Updated SN-quick.py

--- python_cl/sn_quick.py
+++ python_cl/sn_quick.py
@@ -14,8 +14,10 @@ parser.add_argument('-xs',action='store',dest='xs') # Which matrix multiply DJIN
 parser.add_argument('-label',action='store',dest='label') # If the DJINN model is with labeled data
 parser.add_argument('-track',action='store',dest='track') # Tracking the fission and scattering data for DJINN iterations
 parser.add_argument('-problem',action='store',dest='problem') # Problem set up
-parser.add_argument('-model',action='store',dest='model')
+parser.add_argument('-fmodel',action='store',dest='fmodel')
+parser.add_argument('-smodel',action='store',dest='smodel')
 parser.add_argument('-gpu',action='store',dest='gpu')
+# parser.add_argument('-scatter',action='store',dest='scatter')
 usr_input = parser.parse_args()
 
 if usr_input.gpu is None:
@@ -25,19 +27,35 @@ else:
     print('Using GPU')
 
 enrich = [float(jj) for jj in usr_input.en]
+labels = [str(jj).split('.')[1] for jj in enrich]
+
 if usr_input.dist is None:
     print('Using Default Dimensions')
+
 file1 = usr_input.xs
-# label = str(enrich).split('.')[1]
-labels = [str(jj).split('.')[1] for jj in enrich]
+model = usr_input.smodel
 
 process = None
 
-if 'norm' in usr_input.model:
+try:
+    ffile2 = usr_input.fmodel+'/'
+except TypeError:
+    ffile2 = ''
+try:
+    sfile2 = usr_input.smodel+'/'
+except TypeError:
+    sfile2 = ''
+
+if file1 == "scatter":
+    file2 = sfile2
+elif file1 == 'fission':
+    file2 = ffile2
+    model = usr_input.fmodel
+
+if 'norm' in model:
     process = 'norm'
     print('Norm Process')
-file2 = usr_input.model+'/'
-        
+
 if usr_input.label is None:
     file3 = '_reg'
 else:
@@ -45,17 +63,19 @@ else:
 
 if file1 == 'both':
     djinn_model = []    
-    nums = nnets.djinn_metric('{}_1d/{}djinn{}/error/model*'.format('scatter',file2,file3),clean=True)
-    djinn_model.append('{}_1d/{}djinn{}/model_{}'.format('scatter',file2,file3,nums))
-    nums = nnets.djinn_metric('{}_1d/{}djinn{}/error/model*'.format('fission',file2,file3),clean=True) #space should be file2
-    djinn_model.append('{}_1d/{}djinn{}/model_{}'.format('fission',file2,file3,nums))
+    nums = nnets.djinn_metric('{}_1d/{}djinn{}/error/model*'.format('scatter',sfile2,file3),clean=True)
+    djinn_model.append('{}_1d/{}djinn{}/model_{}'.format('scatter',sfile2,file3,nums))
+    nums = nnets.djinn_metric('{}_1d/{}djinn{}/error/model*'.format('fission',ffile2,file3),clean=True) #space should be file2
+    djinn_model.append('{}_1d/{}djinn{}/model_{}'.format('fission',ffile2,file3,nums))
 else:
     nums = nnets.djinn_metric('{}_1d/{}djinn{}/error/model*'.format(file1,file2,file3),clean=True)
+    # if usr_input.scatter is not None:
+    #     nums = str(usr_input.scatter)
     djinn_model = '{}_1d/{}djinn{}/model_{}'.format(file1,file2,file3,nums)
+
 print('DJINN Model',djinn_model)
 
 sprob = '{}_full'.format(usr_input.problem)
-print(usr_input.model,sprob,usr_input.problem)
 
 for ii in range(len(enrich)):
     enrichment,splits = r.problem.boundaries(enrich[ii],distance=usr_input.dist,ptype1=usr_input.problem,ptype2=sprob,symm=True)
@@ -69,9 +89,8 @@ for ii in range(len(enrich)):
         # np.save('mydata/djinn_{}_1d/{}_{}{}_{:<02}'.format(file1,file1,usr_input.normed,file3,labels[ii]),track_fission)
         # np.save('mydata/djinn_{}_1d/{}_{}{}_{:<02}'.format(file1,file1,usr_input.normed,file3,labels[ii]),track_scatter)
     phi,keff = problem.transport(djinn_model,process=process,ptype=usr_input.problem,LOUD=True)
-    np.save('mydata/{}/{}_phi_{}_{:<02}'.format(usr_input.model,file1,file3,labels[ii]),phi)
-    np.save('mydata/{}/{}_keff_{}_{:<02}'.format(usr_input.model,file1,file3,labels[ii]),keff)
-    #np.save('mydata/djinn_{}_1d/phi_{}{}_{:<02}'.format(file1,usr_input.model,file3,labels[ii]),phi)
-    #np.save('mydata/djinn_{}_1d/keff_{}{}_{:<02}'.format(file1,usr_input.model,file3,labels[ii]),keff)
+    np.save('mydata/{}/{}_phi{}_{:<02}'.format(model,file1,file3,labels[ii]),phi)
+    np.save('mydata/{}/{}_keff{}_{:<02}'.format(model,file1,file3,labels[ii]),keff)
+
 
     
