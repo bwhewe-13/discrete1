fd6995486ecbc028222986489d240e6be7672087
Ben <ben.whewell@engineer.com>
2020-12-02 20:28:23 -0500

    Updated autoencoder scripts

--- scripts/ae_retrain.py
+++ scripts/ae_retrain.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
 import numpy as np
-# import discrete1.rogue as r
 from discrete1.util import nnets
-# import tensorflow as tf
 from tensorflow import keras
-# from tensorflow.keras.layers import Input,Dense
-# from tensorflow.keras.models import Model
-# from tensorflow.keras import regularizers
-import argparse
-import os
+import argparse, os, glob
+from sklearn import model_selection
+from tensorflow.keras import Sequential,Model
+from tensorflow.keras.layers import Input,concatenate
+
 
 parser = argparse.ArgumentParser(description='Data Input')
 parser.add_argument('-nodes',action='store',dest='en',nargs='+') #Enrichment looking into
-parser.add_argument('-data',action='store',dest='data') # Data file name
-parser.add_argument('-model',action='store',dest='model')
+parser.add_argument('-data',action='store',dest='data') # phi, smult, or fmult (values)
+parser.add_argument('-model',action='store',dest='model') # type of material
+parser.add_argument('-count',action='store',dest='count') # iteration to use
 parser.add_argument('-gpu',action='store',dest='gpu')
 usr_input = parser.parse_args()
 
@@ -26,32 +25,36 @@ else:
 
 nodes = [int(jj) for jj in usr_input.en]
 file1 = '-'.join([str(jj) for jj in usr_input.en])
-mymat = np.load('mydata/ae_model_data/{}.npy'.format(usr_input.data))
 
-split = 0.2
-try:
-    spatial = np.load('autoencoder/{}/spatialShuffleMat.npy'.format(usr_input.model))
-except FileNotFoundError:
-    spatial = np.arange(len(mymat))
-    np.random.shuffle(spatial)
-    np.save('autoencoder/{}/spatialShuffleMat'.format(usr_input.model),spatial)
+address = np.sort(glob.glob('mydata/ae_model_data/{}_enrich*'.format(usr_input.data)))
+mymat = nnets.randomize(address,150000); dim = 618
+
+# mymat = np.load('mydata/ae_model_data/{}_{}.npy'.format(usr_input.data,usr_input.model)); dim = 87
 # Normalize the data
-mymat,maxi,mini = nnets.normalize(mymat,verbose=True)
+mymat = nnets.normalize(mymat)
 mymat[np.isnan(mymat)] = 0
 
-index = int(len(spatial)*split)
-train = mymat[spatial[index:]]; test = mymat[spatial[:index]]
+train,test = model_selection.train_test_split(mymat,test_size=0.2)
 
-autoencoder = keras.models.load_model('autoencoder/{}/model{}_autoencoder2.h5'.format(usr_input.model,file1))
+prev_label = str(int(usr_input.count) - 1).zfill(3)
+curr_label = str(usr_input.count).zfill(3)
+print('Training Iteration {} from Iteration {}'.format(curr_label,prev_label))
 
-# encoder = Model(autoencoder.input, autoencoder.layers[-2].output)
-# decoder_input = Input(shape=(nodes[-1],))
-# decoder = Model(decoder_input, autoencoder.layers[-1](decoder_input))
+# autoencoder = keras.models.load_model('autoencoder/{}/model_{}_{}_autoencoder_{}.h5'.format(usr_input.model,file1,usr_input.data,prev_label))
+encoder = keras.models.load_model('autoencoder/{}/model_{}_{}_encoder_{}.h5'.format(usr_input.model,file1,usr_input.data,prev_label))
+decoder = keras.models.load_model('autoencoder/{}/model_{}_{}_decoder_{}.h5'.format(usr_input.model,file1,usr_input.data,prev_label))
+
+
+input_shape = Input(shape=(dim,))
+autoencoder = Model(input_shape,decoder(encoder(input_shape)))
+
 
 autoencoder.compile(optimizer='adam',loss='mse')
-history = autoencoder.fit(train,train,epochs=10000,validation_data=(test,test))
+autoencoder.summary()
+history = autoencoder.fit(train,train,epochs=100,validation_data=(test,test))
 
-autoencoder.save('autoencoder/{}/model{}_autoencoder3.h5'.format(usr_input.model,file1))
-# encoder.save('autoencoder/{}/model{}_encoder2.h5'.format(usr_input.model,file1))
-# decoder.save('autoencoder/{}/model{}_decoder2.h5'.format(usr_input.model,file1))
-np.save('autoencoder/{}/loss3_{}'.format(usr_input.model,file1),history.history['loss'])
+autoencoder.save('autoencoder/{}/model_{}_{}_autoencoder_{}.h5'.format(usr_input.model,file1,usr_input.data,curr_label))
+encoder.save('autoencoder/{}/model_{}_{}_encoder_{}.h5'.format(usr_input.model,file1,usr_input.data,curr_label))
+decoder.save('autoencoder/{}/model_{}_{}_decoder_{}.h5'.format(usr_input.model,file1,usr_input.data,curr_label))
+np.save('autoencoder/{}/loss_{}_{}_{}'.format(usr_input.model,file1,usr_input.data,curr_label),history.history['loss'])
+np.save('autoencoder/{}/val_loss_{}_{}_{}'.format(usr_input.model,file1,usr_input.data,curr_label),history.history['val_loss'])
