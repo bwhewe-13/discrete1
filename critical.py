cfc1c6ce5e2e07dedd7ee68f7fd277a6f9337705
Ben <ben.whewell@engineer.com>
2021-03-29 13:54:39 -0400

    Added spherical to critical

--- critical.py
+++ critical.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 class Critical:
     # Keyword Arguments allowed currently
-    __allowed = ("boundary","track")
+    __allowed = ("boundary","track","geometry")
 
     def __init__(self,G,N,mu,w,total,scatter,fission,I,delta,**kwargs):
         """ Deals with Eigenvalue multigroup problems (reflected and vacuum boundaries)
@@ -29,6 +29,7 @@ class Critical:
             boundary: str (default reflected), determine RHS of problem
                 options: 'vacuum', 'reflected'
             track: bool (default False), if track flux change with iteration
+            geometry: str (default slab), determines coordinates of sweep ('slab' or 'spherical')
         """ 
         # Attributes
         self.G = G; self.N = N
@@ -39,12 +40,15 @@ class Critical:
         self.I = I
         self.delta = 1/delta
         self.boundary = 'reflected'; self.track = False
+        self.atype = ''; self.saving = '0'
+        self.geometry = 'slab'
         for key, value in kwargs.items():
-            assert (key in self.__class__.__allowed), "Attribute not allowed, available: boundary, track" 
+            assert (key in self.__class__.__allowed), "Attribute not allowed, available: boundary, track, geometry" 
             setattr(self, key, value)
 
     @classmethod
     def run(cls,refl,enrich,orient='orig',**kwargs):
+        # This is for running the preset eigenvalue problems
         if refl in ['hdpe','ss440']:
             attributes = Problem1.steady(refl,enrich,orient)
         elif refl in ['pu']:
@@ -113,7 +117,7 @@ class Critical:
         return problem.transport(models=model)
 
         
-    def one_group(self,total_,scatter_,source_,guess,tol=1e-08,MAX_ITS=100):
+    def slab(self,total_,scatter_,source_,guess,tol=1e-12,MAX_ITS=100):
         """ Arguments:
             total_: I x 1 vector of the total cross section for each spatial cell
             scatter_: I x L+1 array for the scattering of the spatial cell by moment
@@ -161,6 +165,61 @@ class Critical:
             phi_old = phi.copy()
 
         return phi
+
+    def spherical(self,total_,scatter_,source_,guess,tol=1e-12,MAX_ITS=100):
+        """ Arguments:
+            total_: I x 1 vector of the total cross section for each spatial cell
+            scatter_: I x L+1 array for the scattering of the spatial cell by moment
+            source_: I array for the external sources
+            guess: Initial guess of the scalar flux for a specific energy group (I x L+1)
+        Returns:
+            phi: a I array  """
+        clibrary = ctypes.cdll.LoadLibrary('./discrete1/data/cCriticalSP.so')
+        sweep = clibrary.vacuum
+
+        edges = np.cumsum(np.insert(np.ones((self.I))*1/self.delta,0,0))
+        SA_plus = Critical.surface_area(edges[1:]).astype('float64')       # Positive surface area
+        SAp_ptr = ctypes.c_void_p(SA_plus.ctypes.data)
+
+        SA_minus = Critical.surface_area(edges[:self.I]).astype('float64') # Negative surface area
+        SAm_ptr = ctypes.c_void_p(SA_minus.ctypes.data)
+
+        V = Critical.volume(edges[1:],edges[:self.I])                      # Volume and total
+        v_total = (V * total_).astype('float64')
+        v_ptr = ctypes.c_void_p(v_total.ctypes.data)
+
+        phi_old = guess.copy()
+
+        mu = self.mu.astype('float64')                                     # Angle and weight
+        mu_ptr = ctypes.c_void_p(mu.ctypes.data)
+        w = self.w.astype('float64')
+        w_ptr = ctypes.c_void_p(w.ctypes.data)
+
+        converged = 0; count = 1; 
+
+        while not (converged):
+            phi = np.zeros((self.I),dtype='float64')
+            if self.atype in ['scatter','both']:
+                temp = scatter_
+            else:
+                temp = scatter_ * phi_old
+            psi_nhalf = (Critical.half_angle(0,total_,1/self.delta, source_ + temp)).astype('float64')
+
+            Q = (V * (source_ + temp)).astype('float64')
+            q_ptr = ctypes.c_void_p(Q.ctypes.data)
+
+            psi_ptr = ctypes.c_void_p(psi_nhalf.ctypes.data)
+            phi_ptr = ctypes.c_void_p(phi.ctypes.data)
+
+            sweep(phi_ptr,psi_ptr,q_ptr,v_ptr,SAp_ptr,SAm_ptr,mu_ptr,w_ptr)
+                
+            change = np.linalg.norm((phi - phi_old)/phi/(self.I))
+            converged = (change < tol) or (count >= MAX_ITS) 
+            count += 1
+            phi_old = phi.copy()
+
+        return phi
+
     
     def update_q(self,phi,start,stop,g):
         return np.sum(self.scatter[:,g,start:stop]*phi[:,start:stop],axis=1)
@@ -168,20 +227,25 @@ class Critical:
     def multi_group(self,source,guess,tol=1e-08,MAX_ITS=100):
         phi_old = guess.copy()
 
+        geo = getattr(Critical,self.geometry)  # Get the specific sweep
+
         converged = 0; count = 1
         while not (converged):
             phi = np.zeros(phi_old.shape)
             if self.atype in ['scatter','both']:
+                phi_old[np.isnan(phi_old)] = 0
                 smult = Critical.sorting_scatter(self,phi_old,self.models)
+                # print('here',count,np.sum(phi_old))
                 for g in range(self.G):
-                    phi[:,g] = Critical.one_group(self,self.total[:,g],smult[:,g],source[:,g],phi_old[:,g])
+                    phi[:,g] = geo(self,self.total[:,g],smult[:,g],source[:,g],phi_old[:,g])
             else:
                 for g in range(self.G):
                     q_tilde = source[:,g] + Critical.update_q(self,phi_old,g+1,self.G,g)
                     if g != 0:
                         q_tilde += Critical.update_q(self,phi,0,g,g)
 
-                    phi[:,g] = Critical.one_group(self,self.total[:,g],self.scatter[:,g,g],q_tilde,phi_old[:,g])
+                    # phi[:,g] = Critical.slab(self,self.total[:,g],self.scatter[:,g,g],q_tilde,phi_old[:,g])
+                    phi[:,g] = geo(self,self.total[:,g],self.scatter[:,g,g],q_tilde,phi_old[:,g])
 
             change = np.linalg.norm((phi - phi_old)/phi/(self.I))
             converged = (change < tol) or (count >= MAX_ITS) 
@@ -189,7 +253,7 @@ class Critical:
             phi_old = phi.copy()
         return phi
             
-    def transport(self,models=None,tol=1e-12,MAX_ITS=10):
+    def transport(self,models=None,tol=1e-12,MAX_ITS=100):
 
         self.models = models
         # if self.saving != '0': # Running from random
@@ -202,7 +266,6 @@ class Critical:
         converged = 0; count = 1
         while not (converged):
             sources = Critical.sorting_fission(self,phi_old,self.models)
-
             phi_old = Critical.sorting_phi(self,phi_old,self.models)
 
             print('Outer Transport Iteration {}\n==================================='.format(count))
@@ -225,6 +288,8 @@ class Critical:
         if self.saving == '0' or self.atype not in ['both','fission']: # No Reduction
             return np.einsum('ijk,ik->ij',self.fission,phi)
         elif self.saving in ['1','3']:                                 # DJINN / DJINN + Autoencoder
+            print('right path - sorting fission')
+            # return np.einsum('ijk,ik->ij',self.fission,phi)
             return models.predict_fission(phi)
         elif self.saving in ['2']:                                     # Autoencoder Squeeze
             return models.squeeze(np.einsum('ijk,ik->ij',self.fission,phi))
@@ -240,6 +305,19 @@ class Critical:
             return models.squeeze(phi)
         else:
             return phi
+
+    def surface_area(rho):
+        return 4 * np.pi * rho**2
+
+    def volume(plus,minus):
+        return 4 * np.pi / 3 * (plus**3 - minus**3)
+
+    def half_angle(psi_plus,total,delta,source):
+        """ This is for finding the half angle (N = 1/2) at cell i """
+        psi_half = (2 * psi_plus + delta * source ) / (2 + total * delta)
+        # psi_half /= (2 + total * delta)
+        
+        return psi_half
 
 
     # def tracking_data(self,flux,sources=None):
---
+++ data/cCriticalSP.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LENGTH 1000
+#define N 16
+
+
+
+void vacuum(void *flux, void *half, void *external, void *v_total, void *area_one, void *area_two, void *dir, void *wgt){
+  // Scalar and angular flux
+  double * phi = (double *) flux;
+  double * psi_nhalf = (double *) half;
+  // Scattering and Total Terms
+  double * source = (double *) external;
+  double * total = (double *) v_total;
+  // Surface Area
+  double * SA_plus = (double *) area_one;
+  double * SA_minus = (double *) area_two; 
+  // Angles
+  double * mu = (double *) dir;
+  double * w = (double *) wgt; 
+  
+  // double * center = (double *) middle;
+
+  double psi, psi_ihalf;
+  double alpha_plus;
+
+  double center[N] = {0};
+  double alpha_minus = 0.; // For first angle
+
+  for(int n=0; n < N; n++){
+
+    if (n == N - 1){
+      alpha_plus = 0.;
+    } 
+    else {
+      alpha_plus = alpha_minus - (mu[n] * w[n]);
+    }
+
+    // Sweep from edge to center
+    if (mu[n] < 0){
+          
+      psi_ihalf = 0; // Vacuum at edge
+
+      for(unsigned ii = (LENGTH); ii-- > 0; ){
+        psi = (-1 * mu[n] * (SA_plus[ii] + SA_minus[ii]) * psi_ihalf + 1/w[n] * (SA_plus[ii] - SA_minus[ii]) 
+              * (alpha_plus + alpha_minus) * psi_nhalf[ii] + source[ii]) /
+              (-2 * mu[n] * SA_minus[ii] + 2/w[n] * (SA_plus[ii] - SA_minus[ii]) * 
+                  (alpha_plus) + total[ii]);
+        
+        if(ii == 0){
+          center[n] = psi;
+        }
+        // Update flux
+        phi[ii] = phi[ii] + (w[n] * psi);
+        // Update spatial and angle bounds
+        psi_ihalf = 2 * psi - psi_ihalf;
+        psi_nhalf[ii] = 2 * psi - psi_nhalf[ii];
+
+      }
+    }
+
+    else if (mu[n] > 0){
+
+      psi_ihalf = center[N-n-1]; // Reflected at center
+      // psi_ihalf = 0;
+
+      for(int ii=0; ii < LENGTH; ii++){
+        psi = (mu[n] * (SA_plus[ii] + SA_minus[ii]) * psi_ihalf + 1/w[n] * (SA_plus[ii] - SA_minus[ii]) 
+               * (alpha_plus + alpha_minus) * psi_nhalf[ii] +  source[ii]) / 
+              (2 * mu[n] * SA_plus[ii] + 2/w[n] * (SA_plus[ii] - SA_minus[ii]) * 
+                  (alpha_plus) + total[ii]);
+        // Update spatial cell at middle
+        if(ii == 0){
+          center[n] = psi;
+        }
+        // Update flux
+        phi[ii] = phi[ii] + (w[n] * psi);
+        // Update spatial and angle bounds
+        psi_ihalf = 2 * psi - psi_ihalf;
+        psi_nhalf[ii] = 2 * psi - psi_nhalf[ii];
+
+      }
+    }
+  alpha_minus = alpha_plus;
+
+  }
+
+}
+
+void one_direction(void *flux, void *half, void *external, void *v_total, void *area_one, void *area_two, double w, double mu, double alpha_plus, double alpha_minus, int sweep){
+  // Scalar and angular flux
+  double * phi = (double *) flux;
+  double * psi_nhalf = (double *) half;
+  // Scattering and Total Terms
+  double * source = (double *) external;
+  double * total = (double *) v_total;
+  // Surface Area
+  double * SA_plus = (double *) area_one;
+  double * SA_minus = (double *) area_two; 
+  
+  double psi_ihalf = 0;
+  double psi = 0;
+
+  // Sweep from edge to center
+  if(sweep == 1){
+
+    psi_ihalf = 0; // Vacuum at edge
+
+    for(int ii=0; ii < LENGTH; ii++){
+      psi = (mu * (SA_plus[ii] + SA_minus[ii]) * psi_ihalf + 1/w * (SA_plus[ii] - SA_minus[ii]) 
+            * (alpha_plus + alpha_minus) * psi_nhalf[ii] + source[ii]) /
+            (2 * mu * SA_plus[ii] + 2/w * (SA_plus[ii] - SA_minus[ii]) * 
+                (alpha_plus) + total[ii]);
+
+      
+      // Update flux
+      phi[ii] += (w * psi);
+      // phi[ii] = 1;
+      // Update spatial and angle bounds
+      psi_ihalf = 2 * psi - psi_ihalf;
+      psi_nhalf[ii] = 2 * psi - psi_nhalf[ii];
+    }
+  }
+
+  else if (sweep == -1){
+
+    psi_ihalf = 0; // Vacuum at edge
+
+    for(unsigned ii = (LENGTH); ii-- > 0; ){
+      psi = (-1 * mu * (SA_plus[ii] + SA_minus[ii]) * psi_ihalf + 1/w * (SA_plus[ii] - SA_minus[ii]) 
+            * (alpha_plus + alpha_minus) * psi_nhalf[ii] + source[ii]) /
+            (-2 * mu * SA_minus[ii] + 2/w * (SA_plus[ii] - SA_minus[ii]) * 
+                (alpha_plus) + total[ii]);
+      // Update flux
+      phi[ii] += (w * psi);
+      // Update spatial and angle bounds
+      psi_ihalf = 2 * psi - psi_ihalf;
+      psi_nhalf[ii] = 2 * psi - psi_nhalf[ii];
+    }
+  }
+
+}
