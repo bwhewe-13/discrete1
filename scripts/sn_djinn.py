e70d55f1c4dbcae8245ee61b7137ab1d630d310f
Ben <ben.whewell@engineer.com>
2021-06-26 15:09:18 -0400

    Added more information to time tracking

--- scripts/sn_djinn.py
+++ scripts/sn_djinn.py
@@ -4,15 +4,20 @@ import numpy as np
 from discrete1.critical import Critical
 import argparse, os
 import time
+import datetime
 
 parser = argparse.ArgumentParser(description='Enrichment')
 
 parser.add_argument('-enrich',action='store',dest='en',nargs='+')
 parser.add_argument('-time',action='store',dest='its')
 parser.add_argument('-iteration',action='store',dest='iters')
+parser.add_argument('-reduce',action='store',dest='reduce')
 usr_input = parser.parse_args()
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+day = datetime.datetime.now().strftime('%x').replace('/','-')
+print('Today is ',day)
 
 # Enrichment and associated labels
 enrich = [float(jj) for jj in usr_input.en]
@@ -23,15 +28,27 @@ labels = [str(jj).split('.')[1] for jj in enrich]
 save_folder = 'mydata/djinn_pluto/true'
 
 if usr_input.its:
+    key = np.array(['day','time','type','elapsed','enrichment'])
+    ktype = 'Reduce' if usr_input.reduce else 'Full'
     its = int(usr_input.its)
     for ii in range(len(enrich)):
-        timer = []
+        # timer = []
         for tt in range(its):
             start = time.time()
-            _,_ = Critical.run('pu',enrich[ii])
+            hour = datetime.datetime.now().strftime('%X')
+            if usr_input.reduce:
+                _,_ = Critical.run_reduce('pu',enrich[ii])
+            else:
+                _,_ = Critical.run('pu',enrich[ii])
             end = time.time()
-            timer.append(end-start)
-        np.save('{}_time_{}_{:<02}'.format(save_folder,usr_input.iters,labels[ii]),timer)
+            # timer.append(end-start)
+            key = np.vstack((key,[day,hour,ktype,end-start,labels[ii]]))
+        # np.save('{}_time_{}_{:<02}'.format(save_folder,usr_input.iters,labels[ii]),timer)
+    # Add to key
+    full_key_name = 'true_reduce_time.npy' if usr_input.reduce else 'true_full_time.npy'
+    full_key = np.load(full_key_name)
+    full_key = np.vstack((full_key,key[1:]))
+    np.save(full_key_name,full_key)
 else:
     for ii in range(len(enrich)):
         _,_ = Critical.run('pu',enrich[ii]) # focus=usr_input.focus,
